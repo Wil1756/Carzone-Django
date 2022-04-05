642d506a0a26e763a2a5e5b66d4c101753a19709
john williams <johnwilliams1756@gmail.com>
2022-04-05 12:49:41 +0300

    first commit

---
+++ cars/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Car
+from django.utils.html import format_html
+
+# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.car_photo.url))
+   
+    thumbnail.short_description = 'Car Image'
+
+    list_display =('id','car_title','thumbnail','city','colour','model','year','body_style','fuel_type','is_featured')
+    list_display_links  =('id','car_title','thumbnail')
+    list_editable =('is_featured',)
+    search_fields =('car_title','city','model','body_style','fuel_type')
+    list_filter =('car_title','city','model','body_style','fuel_type','year')
+
+admin.site.register(Car,CarAdmin)
+
